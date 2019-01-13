2b3e544fea574ea0df612ca670158ec85e6bb881
Feluin <pascal.roecker.1997@gmail.com>
2019-01-13 18:53:10 +0100

    Moved something to somewhere... goodnight...

---
+++ src/main/airport/AirCraftEvents/TaxiEvent.java
@@ -0,0 +1,4 @@
+package airport.AirCraftEvents;
+
+public class TaxiEvent implements AirCraftEvent {
+}
--- src/main/airport/Airport.java
+++ src/main/airport/Airport.java
@@ -1,6 +1,10 @@
 package airport;
 
+import java.util.*;
+
 public class Airport
 {
     private String name;
+    List<Gate.GateNames> gates=new ArrayList<>(Arrays.asList(Gate.GateNames.values()));
+    Map<Gate,Aircraft> gateairportmap=new HashMap<>();
 }
