715ad885454478545411ccf88ab86a1cfc1c5338
Feluin <pascal.roecker.1997@gmail.com>
2019-01-13 19:42:40 +0100

    Fixed so the code compiles

--- src/main/airport/Airport.java
+++ src/main/airport/Airport.java
@@ -7,4 +7,18 @@ public class Airport
     private String name;
     List<Gate.GateNames> gates=new ArrayList<>(Arrays.asList(Gate.GateNames.values()));
     Map<Gate,Aircraft> gateairportmap=new HashMap<>();
+    List<Aircraft> aircrafts=new ArrayList<>();
+    private static Airport airport=new Airport();
+    private Tower tower;
+    private ApronControl apronControl;
+
+    private Airport(){
+        tower=new Tower();
+        apronControl= new ApronControl();
+    }
+
+    public static Airport getInstance() {
+        return airport;
+    }
+
 }
