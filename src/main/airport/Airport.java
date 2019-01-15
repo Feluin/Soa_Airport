6f65f8943435286273e4d0a9528906683366fbb3
Raphael Lawo <RaphaelLawo@gmx.de>
2019-01-15 09:04:09 +0100

    best commit ever

--- src/main/airport/Airport.java
+++ src/main/airport/Airport.java
@@ -8,6 +8,18 @@ public class Airport
 {
     private String name;
     private List<Gate.GateNames> gates=new ArrayList<>(Arrays.asList(Gate.GateNames.values()));
-    private Map<Gate, Aircraft> gateairportmap=new HashMap<>();
+    private Map<Gate, Aircraft> gateAirportMap=new HashMap<>();
+
+    public Airport(String name) {
+        this.name = name;
+    }
+
+    public List<Gate.GateNames> getGates() {
+        return gates;
+    }
+
+    public Map<Gate, Aircraft> getGateAirportMap() {
+        return gateAirportMap;
+    }
 
 }
